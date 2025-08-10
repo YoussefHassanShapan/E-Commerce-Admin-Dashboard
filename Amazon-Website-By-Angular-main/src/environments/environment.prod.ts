062ce7618f659668c67b2a86e229b06aa4c93aa6
Youssef Hassan <youssefhassanshapan1995@gmail.com>
2025-08-10 22:30:37 +0300

    third app

---
+++ Amazon-Website-By-Angular-main/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // APIBaseURL:'http://www.amazon-api.somee.com'
+  // APIBaseURL:'https://localhost:7027'
+  APIBaseURL:'https://localhost:7027'
+
+};
